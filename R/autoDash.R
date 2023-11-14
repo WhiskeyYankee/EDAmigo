3e00e45104da0e36ff9d41ced36d021eead6c559
Allison Moore <amoore214@tamu.edu>
2023-11-14 18:14:09 -0500

    Update with Dashboard

---
+++ R/autoDash.R
@@ -0,0 +1,21 @@
+#' autoDash
+#'
+#' Automatically create an interactive dashboard
+#'
+#' @param df A dataframe, mixed data types allowed.
+#'
+#' @return Outputs an interactive HTML dashbaoard.
+#' @export
+#'
+#' @examples
+#' autoDash(finance)
+#'
+autoDash <- function(df){
+
+  rmarkdown::render("inst/Dashboard.Rmd", params= list(x= df$loan_amnt, y= df$funded_amnt,
+                  pie = df[, c('term', 'loan_amnt')], box =df[,c('funded_amnt' , 'term')]))
+  output_html <- "inst/Dashboard.html"
+
+  # Open the HTML file in the default web browser
+  browseURL(paste("file://", normalizePath(output_html), sep=""))
+}
