535959743e6d11fc8c6005f12497ed28e642519a
Allison Moore <amoore214@tamu.edu>
2023-11-20 11:48:08 -0500

    detectMissing in progress. Documentation update.

--- R/autoClean.R
+++ R/autoClean.R
@@ -1,13 +1,13 @@
 #' autoClean
 #'
-#' @param df
-#' @param dateForm
-#' @param cat_tol
-#' @param user_tol
-#' @param vals
-#' @param user_level
+#' @param df A dataframe.
+#' @param dateForm A string, or vector of strings, indicating the format of any dates contained in the dataframe.
+#' @param cat_tol A numeric value indicating the tolerance of unique integer values to automatically coerce a column to a categorical variable. Report in percent from 0 to 100.
+#' @param user_tol A numeric value indicating the tolerance to receive user input prompts to assist in selecting which integer columns should be coerced to categorical, date, or retain as an integer. Report in percent from 0 to 100.
+#' @param vals A gsub formatted list of characters to keep or remove.
+#' @param user_level An indicator of whether the user will provide input or if the user would like to fully automate the process, 1 indicates user interaction.
 #'
-#' @return
+#' @return A dataframe with special characters removed and column types set to the appropriate class.
 #' @export
 #'
 #' @examples
