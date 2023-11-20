97f14f9a3304bfd3f977a2fdf60bab85e361bd64
Allison Moore <amoore214@tamu.edu>
2023-11-20 12:02:10 -0500

    update documentation.

--- R/autoClean.R
+++ R/autoClean.R
@@ -5,19 +5,21 @@
 #' @param cat_tol A numeric value indicating the tolerance of unique integer values to automatically coerce a column to a categorical variable. Report in percent from 0 to 100.
 #' @param user_tol A numeric value indicating the tolerance to receive user input prompts to assist in selecting which integer columns should be coerced to categorical, date, or retain as an integer. Report in percent from 0 to 100.
 #' @param vals A gsub formatted list of characters to keep or remove.
+#' @param drop_tol A percent tolerance to automatically drop columns with percent missing values greater than or equal to this value.
 #' @param user_level An indicator of whether the user will provide input or if the user would like to fully automate the process, 1 indicates user interaction.
 #'
 #' @return A dataframe with special characters removed and column types set to the appropriate class.
 #' @export
 #'
 #' @examples
+#' cleaned <- autoClean(fires)
 autoClean <- function(df, dateForm = "%m/%d/%Y", cat_tol = NULL, user_tol = 80, vals = "[^0-9A-Za-z.,[:space:]-]", drop_tol = NULL, user_level = 0){
 
-    clearSpecial <- handleSpecial(df, vals, user_level)
+    clearSpecial <- handleSpecial(df, vals, user_level)$df
 
     set_types <- detectTypes(clearSpecial, dateForm, cat_tol, user_tol)
 
-    clean <- detectMissing(df, drop_tol , user_level)
+    clean <- detectMissing(df, drop_tol , user_level)$df
 
     return(clean)
 
