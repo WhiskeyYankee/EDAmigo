748f5287fc7b5a49802a6cc9c324ba3501890720
Allison Moore <amoore214@tamu.edu>
2023-11-25 10:41:08 -0500

    Reorder type order. Edit function examples.

--- R/detectTypes.R
+++ R/detectTypes.R
@@ -29,17 +29,22 @@ detectTypes <- function(df, factor_tol = NULL, user_tol = 30){
   n <- nrow(df)
   column_order <- names(df)
 
-  # Separate out numeric columns
-  nums <- dplyr::select_if(df, is.numeric)
-
   # Separate out date columns
   dates_times <- df[sapply(df, function(column) inherits(column, 'Date')) | sapply(df, function(column) inherits(column, 'POSIXct')) | sapply(df, function(column) inherits(column, 'POSIXlt'))]
 
-  # Separate out non-numeric columns
-  not_nums <- df[!colnames(df) %in% colnames(nums) & !colnames(df) %in% colnames(dates_times)]
-
   # Define empty dataframe for factors
-  factors <- data.frame(rep(NA, n))
+  factors <- df[sapply(df, function(column) inherits(column, 'factor'))]
+
+  # Separate out numeric columns and coerce numeric strings to numeric
+  nums <- dplyr::select_if(df, is.numeric)
+  coerce_to_num <-  suppressWarnings(.tryNumeric(df[!colnames(df) %in% colnames(dates_times) & !colnames(df) %in% colnames(nums) & !colnames(df) %in% colnames(factors)]))
+  nums <- cbind(nums,dplyr::select_if(coerce_to_num, is.numeric))
+
+
+  # Separate out non-numeric columns
+  not_nums <- df[!colnames(df) %in% colnames(nums) & !colnames(df) %in% colnames(dates_times) & !colnames(df) %in% colnames(factors)]
+
+
 
   # Find percent unique for each column
   typeStats <- data.frame(sapply(df, function(column) (100 * (length(as.vector(stats::na.omit(unique(column)))) / length(stats::na.omit(column))))))
@@ -78,11 +83,15 @@ detectTypes <- function(df, factor_tol = NULL, user_tol = 30){
     to_type = 0
 
     if(typeStats[column, 'percent_unique'] <= user_tol){
-      to_type <- utils::menu(c('Factor', 'Keep as a string!'), title = cat(column, ' contains ', typeStats[column, 'percent_unique'],'% unique values. Is this:'))
+      to_type <- utils::menu(c('Factor', 'Keep as a string', 'Hey! That is actually numeric!'), title = cat(column, ' contains ', typeStats[column, 'percent_unique'],'% unique values. Is this:'))
     }
 
     if (to_type == 1){ # Set column to factor
       factors[[column]] <- as.factor(df[[column]])
+      not_nums <- not_nums[, !(names(not_nums) %in% column)]
+    }
+    if (to_type == 3){ # Set column to numeric
+      nums[[column]] <- as.numeric(df[[column]])
       not_nums <- not_nums[, !(names(not_nums) %in% column)]
     }
 
@@ -93,31 +102,37 @@ detectTypes <- function(df, factor_tol = NULL, user_tol = 30){
   # Iterate through non-numeric columns to request user input for integer coercion to char, factor, using user_tol %
   for (column in names(nums)){
 
-    if (sum(df[[column]] %% 1, na.rm= TRUE) == 0){ # Identify any columns of all integers
+    if (sum(nums[[column]] %% 1, na.rm= TRUE) == 0){ # Identify any columns of all integers
 
       to_type = 0
 
       # If column % unique is less than user_tol, request user input for coercion
       if (typeStats[column, 'percent_unique'] <= user_tol){
-        to_type <- utils::menu(c('Character','Factor', 'Keep as an Integer!'), title = cat(column, ' contains ', typeStats[column, 'percent_unique'],'% unique values. Is this:'))
+        to_type <- utils::menu(c('Character','Factor', 'Float', 'Keep as an Integer!'), title = cat(column, ' contains ', typeStats[column, 'percent_unique'],'% unique values. Is this:'))
       }
 
       if (to_type == 1){ # Set column to character type
-        not_nums[[column]] <- as.character(df[[column]])
+        not_nums[[column]] <- as.character(nums[[column]])
         nums <- nums[, !(names(nums) %in% column)]
       }
 
       if (to_type == 2){ # Set column to factor
-        factors[[column]] <- as.factor(df[[column]])
+        factors[[column]] <- as.factor(nums[[column]])
         nums <- nums[, !(names(nums) %in% column)]
-       }
+      }
+
+      if (to_type == 3){ # Set column to float
+        nums[[column]] <- as.numeric(nums[[column]])
+      }
+
+      if (to_type == 4){ # Set column to int
+        nums[[column]] <- as.integer(nums[[column]])
+      }
 
     } # End if sum
   } # End for column
 
 
-  # Drop the column of NA placeholders from factors
-  factors <- factors[,-1]
 
   # List the new class for each column
   typeStats$new_class <- sapply(cbind(nums, not_nums, dates_times, factors)[column_order], function(column) class(column))
