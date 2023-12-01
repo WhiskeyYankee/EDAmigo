2b038a7f8f9bce193bca6d03e3a75abb642350be
Peter Dimler <peter.dimler@tamu.edu>
2023-11-30 21:44:32 -0500

    Updates checks

--- R/.boxCox_Test.R
+++ R/.boxCox_Test.R
@@ -1,52 +1,67 @@
+## load libaries
+library(testthat)
+
+## Load functions
 source("R/boxCox.R")
 
+# Create toy data set
 test_data =
   data.frame(
-     X_1 = rchisq(1000, df = 1)
-    ,X_2 = rchisq(1000, df = 5)
-  )
+     X_1 = rchisq(1000, df = 10)
+    ,X_2 = rchisq(1000, df = 5))
 
-boxCox(test_data)$boxCox_Results
-### Test 1
-test_bx_1 = MASS::boxcox(data = test_data, X_1~1,lambda = seq(from =-3, to = 3, by = 0.01),plotit = TRUE)
-test_bx_1$x[which.max(test_bx_1$y)]
-test_trn_1 = (test_data$X_1^(-2) - 1)/(-2)
+### Test 1 use the box cox funtion from the mass package to evaluate the data
+mass_bxcx_1 = MASS::boxcox(data = test_data, X_1~1,lambda = seq(from =-3, to = 3, by = 0.01),plotit = TRUE)
 
-### Test 2
-test_bx_2 = MASS::boxcox(data = test_data, X_2~1,lambda = seq(from =-3, to = 3, by = 0.01),plotit = TRUE)
-test_bx_2$x[which.max(test_bx_2$y)]
-test_trn_2 = (test_data$X_2^(1/3) - 1)/(1/3)
+mass_bxcx_2 = MASS::boxcox(data = test_data, X_2~1,lambda = seq(from =-3, to = 3, by = 0.01),plotit = TRUE)
 
-(test_12_1 = boxCox(test_data)$boxCox_Results)
-(test_12_2 = boxCox(test_data, suggest = FALSE)$boxCox_Results)
+mass_results = c(mass_bxcx_1$x[which.max(mass_bxcx_1$y)], mass_bxcx_2$x[which.max(mass_bxcx_2$y)])
+
+EDAmigo_results = boxCox(test_data)
+
+expect_equivalent(mass_results , EDAmigo_results$boxCox_Results$lambda_1)
+
+
 
 #### Test 3
 data("finance")
-test_bx_3 = MASS::boxcox(data = finance, out_prncp~1,lambda = seq(from =-5, to = 5, by = 0.01),plotit = TRUE)
+
+finance$loan_amnt = finance$loan_amnt +0.01
+test_bx_3 = MASS::boxcox(data = finance, loan_amnt~1,lambda = seq(from =-3, to = 3, by = 0.01),plotit = TRUE)
 test_bx_3$x[which.max(test_bx_3$y)]
 
-test_3 = boxCox(finance)
-test_3_1 = boxCox(finance, suggest = TRUE)
+finance_results = boxCox(finance)
 
+finance_results_2 = boxCox(finance, cols = c("total_pymnt_inv", "total_pymnt"))
+finance_results_3 = boxCox(finance, cols = c(37,36))
 ### Test 4
 library(tidyverse)
 library(tidymodels)
-test_4_data  = as.data.frame(state.x77)
-rec <- recipe(~., data = as.data.frame(state.x77))
+X = as.data.frame(state.x77)
 
-bc_trans <- step_BoxCox(rec, all_numeric())
+state_x77  = as.data.frame(state.x77)
+rec <- recipe(~., data = state_x77)
 
-bc_estimates <- prep(bc_trans, training = as.data.frame(state.x77))
+bc_trans <- step_BoxCox(rec, all_numeric(), limits = c(-3,3))
 
-bc_data <- bake(bc_estimates, as.data.frame(state.x77))
+bc_estimates <- prep(bc_trans, training = state_x77)
 
-plot(density(state.x77[, "Illiteracy"]), main = "before")
+bc_data <- bake(bc_estimates, state_x77)
+
+plot(density(state_x77[, "Illiteracy"]), main = "before")
 
 plot(density(bc_data$Illiteracy), main = "after")
 
 
 tidy(bc_trans, number = 1)
-tidy(bc_estimates, number = 1)
 
-test_4 = boxCox(test_4_data)
+tidymodel_est = tidy(bc_estimates, number = 1)
 
+
+EDAmigo_est = boxCox(state_x77, lambda = seq(from =-3, to = 3, by = 0.01))
+
+MASS_results = MASS::boxcox(data = state_x77, Income~1,lambda = seq(from =-5, to = 5, by = 0.01),plotit = TRUE)
+(MASS_results$x[which.max(MASS_results$y)])
+
+
+ENV_results = EnvStats::boxcox(state_x77$Income, lambda = seq(from =-4.5, to = 4.5, by = 0.01), objective.name = "Log-Likelihood")
