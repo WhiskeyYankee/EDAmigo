83df66e7f683d9b6d633ff1330c3cd072f0b88d5
Peter Dimler <peter.dimler@tamu.edu>
2023-11-30 22:32:49 -0500

    merges conflicts

--- R/autoClean.R
+++ R/autoClean.R
@@ -4,15 +4,18 @@
 #' @param factor_tol A numeric value indicating the tolerance of unique integer values to automatically coerce a column to a categorical variable. Report in percent from 0 to 100.
 #' @param user_tol A numeric value indicating the tolerance to receive user input prompts to assist in selecting which integer columns should be coerced to categorical, date, or retain as an integer. Report in percent from 0 to 100.
 #' @param vals A gsub formatted list of characters to keep or remove.
+#' @param no_drop A boolean indicating whether or not to drop values, TRUE skips all dropping and proceeds to imputation.
+#' @param no_impute A boolean indicating whether or not to impute values. TRUE skips imputation.
 #' @param drop_tol A percent tolerance to automatically drop columns with percent missing values greater than or equal to this value.
-#' @param user_level An indicator of whether the user will provide input or if the user would like to fully automate the process, 1 indicates user interaction.
+#' @param special_user_level An indicator of whether the user will provide input when removing special characters or if the user would like to fully automate the process, 1 indicates user interaction.
+#' @param missing_user_level An indicator of whether the user will provide input when handling missing values or if the user would like to fully automate the process, 1 indicates user interaction.
 #'
 #' @return A dataframe with special characters removed and column types set to the appropriate class.
 #' @export
 #'
 #' @examples
 #' cleaned <- autoClean(fires, factor_tol = 10)
-autoClean <- function(df, factor_tol = NULL, user_tol = 30, vals = "[^0-9A-Za-z.,[:space:]-]", drop_tol = NULL, user_level = 0){
+autoClean <- function(df, factor_tol = NULL, user_tol = 30, vals = "[^0-9A-Za-z.,[:space:]-]", no_drop = FALSE,  no_impute = FALSE, drop_col_tol = 50, drop_row_tol = NULL, user_level = 0){
 
   # Handle special characters. Save output as individual variables to pass out.
   specialOutput <- handleSpecial(df, vals, user_level)
@@ -29,7 +32,7 @@ autoClean <- function(df, factor_tol = NULL, user_tol = 30, vals = "[^0-9A-Za-z.
   typeStats <- set_types$type_stats
 
   # Handle missing values, drop or impute.
-  clean <- handleMissing(typed_df, drop_tol , user_level)
+  clean <- handleMissing(typed_df, no_drop, drop_col_tol, drop_row_tol, user_level)
   missingStats <- clean$missing_stats
   cleanDf <- clean$df
 
--- R/handleMissing.R
+++ R/handleMissing.R
@@ -1,28 +1,31 @@
 #' handleMissing
 #'
 #' @param df A dataframe.
-#' @param drop_tol A percent tolerance to automatically drop columns with percent missing values greater than or equal to this value.
-#' @param user_level An indicator of whether the user will provide input or if the user would like to fully automate the process, 1 indicates user interaction.
+#' @param no_drop A boolean indicating whether or not to drop values, TRUE skips all dropping and proceeds to imputation.
+#' @param no_impute A boolean indicating whether or not to impute values. TRUE skips imputation.
+#' @param drop_col_tol A percent tolerance to automatically drop columns with percent missing values greater than or equal to this value.
+#' @param drop_row_tol A percent tolerance to automatically drop rows with percent missing values greater than or equal to this value.If values are provided for both columns and rows, columns will be dropped first.
+#' @param missing_user_level An indicator of whether the user will provide input or if the user would like to fully automate the process, 1 indicates user interaction.
 #'
 #' @return The dataframe with dropped or imputated missing values.
 #' @export
 #'
 #' @examples
 #' # Define the dataframe, clear of special characters
-#' df <- handleSpecial(fires,user_level =0)$df
+#' df <- handleSpecial(fires, special_user_level =0)$df
 #'
 #' # Store function results
-#' out <- handleMissing(df, drop_col_tol = 80, user_level = 0)
+#' out <- handleMissing(df, drop_col_tol = 80, missing_user_level = 0)
 #' str(out$df)
 #'
-handleMissing <- function(df, drop_col_tol = NULL, drop_row_tol = NULL, user_level = 1){
+handleMissing <- function(df, no_drop = FALSE, no_impute = FALSE, drop_col_tol = NULL, drop_row_tol = NULL, missing_user_level = 1){
 
   # Check to make sure user has supplied enough parameters to execute the function. Must specifiy at least one drop tolerance, or utilize user interaction.
-  if(is.null(drop_col_tol) & is.null(drop_row_tol) & user_level == 0) stop("Please supply at least one tolerance, or set user_level to 1.")
+  if(is.null(drop_col_tol) & is.null(drop_row_tol) & missing_user_level == 0) stop("Please supply at least one tolerance, or set missing_user_level to 1.")
 
   # Calculate the percentage of missing values in each column of the dataframe and save information to missing_stats
   # Perform separate calculation on date/time columns
-  percent_missing <- sapply(df, function(column) {
+  initial_percent_missing <- sapply(df, function(column) {
     if (inherits(column, "POSIXlt") | inherits(column, "Date") |inherits(column, "POSIXct")) {
       return(100 * sum(is.na(column)) / length(column))
     } else {
@@ -30,82 +33,141 @@ handleMissing <- function(df, drop_col_tol = NULL, drop_row_tol = NULL, user_lev
     }
   })
 
-  missing_stats <- data.frame(percent_missing = round(percent_missing, 1) )
-
+  missing_stats <- data.frame(variable = names(initial_percent_missing), initial_percent_missing = round(initial_percent_missing, 1) )
   dropped_cols <-c()
   dropped_rows <- c()
 
-  # If user defines a column drop tolerance, drop columns which have greater than or equal value of % missing
-  if (!is.null(drop_col_tol)){
-    below_tolerance <- apply(missing_stats, 1, function(x) ifelse(x[['percent_missing']] < drop_col_tol, x, NA))
-    df <- df[which(names(df) %in% names(stats::na.omit(below_tolerance)))]
-    dropped_cols <- append(dropped_cols, as.vector(names(below_tolerance[is.na(below_tolerance)])))
-  }
-
-  # If user defines a row drop tolerance, drop rows which have greater than or equal value of % missing
-  if (!is.null(drop_row_tol)){
-    df_temp <- .tryNumeric(df)
-    obs_percent_missing <- apply(df_temp, 1, function(x) 100 * sum(is.na(x) | x =='') / length(x))
-    df <- df[which(obs_percent_missing < drop_row_tol), ]
-    dropped_rows <- append(dropped_rows, as.vector(which(obs_percent_missing >= drop_row_tol)))
-  }
+  # If the user wants to drop columns or rows
+  if (no_drop == FALSE){
+    # If user defines a column drop tolerance, drop columns which have greater than or equal value of % missing
+    if (!is.null(drop_col_tol)){
+      remove <- missing_stats$variable[missing_stats$initial_percent_missing  >= drop_col_tol]
+      dropped_cols <- unique(append(dropped_cols, as.vector(remove)))
+      df <- df[, !names(df) %in% dropped_cols]
 
 
+      # Update stats with % missing after dropping
+      after_drop <- data.frame(after_col_drop_percent = round( sapply(df, function(column) {
+        if (inherits(column, "POSIXlt") | inherits(column, "Date") |inherits(column, "POSIXct")) {
+          return(100 * sum(is.na(column)) / length(column))
+        } else {
+          return(100 * sum(is.na(column) | column == '') / length(column))
+        }
+      }), 1))
+      after_drop$variable <- rownames(after_drop)
+      missing_stats <- suppressWarnings(merge(missing_stats, after_drop, by = 'variable', all =TRUE, no.dup=TRUE))
+      missing_stats[is.na(missing_stats)] <- 'dropped'
 
-  if (user_level == 1){
-    print(missing_stats)
-    cat('\n')
-    input <- utils::menu(c("Drop all missing", "Select which to drop", "Set a drop tolerance", "See missing by row"),title = "Above are the percent missing by column for your dataframe. Which would you like to do? ")
 
-    if (input == 1){
-      kept <- apply(missing_stats, 1, function(x) ifelse(x[['percent_missing']] > 0, NA, x))
-      df <- df[which(names(df) %in% names(stats::na.omit(kept)))]
-      dropped_cols <- append(dropped_cols, as.vector(names(kept[is.na(kept)])))
     }
 
-    if (input == 2){
-      options <- sprintf("%-20s %-10s",names(df), round(percent_missing,1))
-      to_drop <- coda::multi.menu(options, title = "Which would you like to drop?")
-
-      if(to_drop != 0){
-        drop_names <- names(df)[to_drop]
-        df <- df[which(!names(df) %in% drop_names)]
-        dropped_cols <- append(dropped_cols, drop_names)
-      }
-    }
-
-    if (input == 3){
-      tol <- readline(prompt = "What tolerance would you like to set? ")
-      kept <- apply(missing_stats, 1, function(x) ifelse(x[['percent_missing']] >= tol, NA, x))
-      df <- df[which(names(df) %in% names(stats::na.omit(kept)))]
-      dropped_cols <- append(dropped_cols, as.vector(names(kept[is.na(kept)])))
-    }
-
-
-    # If the user wants to view percent missing by observation (row)
-    if (input == 4){
-
-      # Coerce dates to numeric to prevent errors and calculate % missing by row
+    # If user defines a row drop tolerance, drop rows which have greater than or equal value of % missing
+    if (!is.null(drop_row_tol)){
       df_temp <- .tryNumeric(df)
-      obs_percent_missing <- apply(df_temp, 1, function(x) 100 * sum(is.na(x) | x =='') / length(x))
+      obs_initial_percent_missing <- apply(df_temp, 1, function(x) 100 * sum(is.na(x) | x =='') / length(x))
+      df <- df[which(obs_initial_percent_missing < drop_row_tol), ]
+      dropped_rows <- append(dropped_rows, as.vector(which(obs_initial_percent_missing >= drop_row_tol)))
+    }
 
-      # Output a histogram of percent missing
-      par(mfrow=c(1,1))
-      hist(obs_percent_missing, main = "Histogram of percent missing by rows", xlab ="Percent missing")
 
-      # Get user feedback on how they would like to progress.
-      row_input <- menu(c("Set row drop tolerance", "Exit Function, I'm done!"), title="Please view the histogram of observation percent missing (by row). What would you like to do now? ")
+    # If the user indicated they want input in the drop process.
+    if (missing_user_level == 1){
+      print(missing_stats)
+      cat('\n')
+      input <- utils::menu(c("Drop all missing", "Select which to drop", "Set a drop tolerance", "See missing by row"),title = "Above are the percent missing by column for your dataframe. Which would you like to do? ")
 
-      # If user would like to set a drop tolerance for rows, take tolerance, remove rows and update dropped_rows
-      if (row_input == 1){
-        row_tol <- readline(prompt = "What tolerance would you like to set? ")
-        df <- df[which(obs_percent_missing < row_tol), ]
-        dropped_rows <- append(dropped_rows,as.vector(which(obs_percent_missing >= row_tol)))
+      # If the user wants to drop all columns with any missing values.
+      if (input == 1){
+        remove <- missing_stats$variable[missing_stats$initial_percent_missing  > 0]
+        dropped_cols <- unique(append(dropped_cols, as.vector(remove)))
+        df <- df[, !names(df) %in% dropped_cols]
+
       }
+
+      # If the user wants to select which columns to drop
+      if (input == 2){
+        if(ncol(missing_stats) == 2){
+         options <- sprintf("%-30s %.1f", missing_stats$variable, missing_stats$initial_percent_missing)
+        }
+        else{
+          options <- sprintf("%-30s %-6s %-4s", missing_stats$variable, missing_stats$initial_percent_missing, missing_stats$after_col_drop_percent)
+          }
+        to_drop <- coda::multi.menu(options, title = "Which would you like to drop?")
+
+        if(sum(to_drop) != 0 ){
+          drop_names <- missing_stats$variable[to_drop]
+          df <- df[which(!names(df) %in% drop_names)]
+          dropped_cols <- append(dropped_cols, drop_names)
+        }
+      }
+
+      # If the user wants to set a column drop tolerance
+      if (input == 3){
+        tol <- readline(prompt = "What tolerance would you like to set? ")
+        remove <- missing_stats$variable[missing_stats$initial_percent_missing >= tol ]
+        dropped_cols <- unique(append(dropped_cols, as.vector(remove)))
+        df <- df[, !names(df) %in% dropped_cols]
+      }
+
+
+      # If the user wants to view percent missing by observation (row)
+      if (input == 4){
+
+        # Coerce dates to numeric to prevent errors and calculate % missing by row
+        df_temp <- .tryNumeric(df)
+        obs_initial_percent_missing <- apply(df_temp, 1, function(x) 100 * sum(is.na(x) | x =='') / length(x))
+
+        # Output a histogram of percent missing
+        par(mfrow=c(1,1))
+        hist(obs_initial_percent_missing, main = "Histogram of percent missing by rows", xlab ="Percent missing")
+
+        # Get user feedback on how they would like to progress.
+        row_input <- menu(c("Set row drop tolerance", "Exit, I don't want to drop any rows!"), title="Please view the histogram of observation percent missing (by row). What would you like to do now? ")
+
+        # If user would like to set a drop tolerance for rows, take tolerance, remove rows and update dropped_rows
+        if (row_input == 1){
+          row_tol <- readline(prompt = "What tolerance would you like to set? ")
+          df <- df[which(obs_initial_percent_missing < row_tol), ]
+          dropped_rows <- append(dropped_rows,as.vector(which(obs_initial_percent_missing >= row_tol)))
+        }
+
+      }
+
+          # Update stats with % missing after dropping
+    after_drop_percent <- data.frame(after_drop_percent = round(sapply(df, function(column) {
+      if (inherits(column, "POSIXlt") | inherits(column, "Date") |inherits(column, "POSIXct")) {
+        return(100 * sum(is.na(column)) / length(column))
+      } else {
+        return(100 * sum(is.na(column) | column == '') / length(column))
+      }
+    }), 1))
+    after_drop_percent$variable <- rownames(after_drop_percent)
+    missing_stats <- suppressWarnings(merge(missing_stats, after_drop_percent, by = 'variable', all =TRUE, no.dups = TRUE))
+    missing_stats[is.na(missing_stats)] <- 'dropped'
 
     }
 
+
+
   }
+
+  # Once drop process complete, or if the user does not want to drop columns or rows
+  if (no_impute == FALSE){
+
+    print(missing_stats)
+
+  }
+
+  # Update stats with final % missing
+  end_percent_missing <- data.frame(end_percent_missing = round( sapply(df, function(column) {
+    if (inherits(column, "POSIXlt") | inherits(column, "Date") |inherits(column, "POSIXct")) {
+      return(100 * sum(is.na(column)) / length(column))
+    } else {
+      return(100 * sum(is.na(column) | column == '') / length(column))
+    }
+  }), 1))
+  missing_stats <- merge(missing_stats, end_percent_missing, by = 'row.names', all =TRUE, no.dups = TRUE)
+  missing_stats[is.na(missing_stats)] <- 'dropped'
 
   return(list('df' = df, 'missing_stats' = missing_stats, 'dropped_cols'= dropped_cols, 'dropped_rows'= dropped_rows))
 
